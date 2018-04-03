01006dcd5bb0ce4e4264eb3b195667b4618b98bd
ElToredo <ascftgbhujj@gmail.com>
2018-04-03 09:51:22 +0200

    First stable version with SFML installed.

---
+++ MiamiOps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiamiOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
