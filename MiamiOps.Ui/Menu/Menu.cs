0af1abff15116e47efdcca2c7c81068b9f08f9d7
Dan <dchiche94@hotmail.fr>
2018-04-09 15:07:03 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- MiamiOps.Ui/Menu/Menu.cs
+++ MiamiOps.Ui/Menu/Menu.cs
@@ -15,9 +15,10 @@ namespace MiamiOps
     {
         private int selectedItemIndex;
         private Text[] menuList = new Text[3];
+        private Texture[] buttonList = new Texture[3];
         private Font font = new Font("../../../Menu/arial.ttf");
-        static Texture _backgroundTexture = new Texture("../../../../Images/cat.png");
-        static Sprite _backgroundSprite;
+        static Texture _backgroundTexture = new Texture("../../../../Images/background.png");
+        //static Sprite _backgroundSprite;
         Music music = new Music("../../../Menu/TheLastOfUs.ogg");
 
         public Menu(float width, float height)
@@ -28,15 +29,17 @@ namespace MiamiOps
                 Font = font,
                 Color = Color.White,
                 DisplayedString = "Nouvelle Partie",
-                Position = new Vector2f(width / 2, height / (3 + 1) * 1)
+                Position = new Vector2f( width/2  - width/10 , height - height / 3),
+                
             };
             menuList[0] = menu1;
+            
             Text menu2 = new Text
             {
                 Font = font,
                 Color = Color.White,
                 DisplayedString = "Charger Partie",
-                Position = new Vector2f(width / 2, height / (3 + 1) * 2)
+                Position = new Vector2f(width / 2 - width / 10, height - height /4)
             };
             menuList[1] = menu2;
 
@@ -45,16 +48,16 @@ namespace MiamiOps
                 Font = font,
                 Color = Color.White,
                 DisplayedString = "Quitter",
-                Position = new Vector2f(width / 2, height / (3 + 1) * 3)
+                Position = new Vector2f(width / 2 - width / 18, height-height/6)
             };
             menuList[2] = menu3;
-           _backgroundSprite = new Sprite(_backgroundTexture);
+         //  _backgroundSprite = new Sprite(_backgroundTexture);
 
         }
 
         public void Draw(RenderWindow window)
         {
-            _backgroundSprite.Draw(window, RenderStates.Default);
+           // _backgroundSprite.Draw(window, RenderStates.Default);
             for (int i = 0; i < menuList.Length; i++)
             {
                 window.Draw(menuList[i]);
@@ -68,14 +71,14 @@ namespace MiamiOps
             {
                 menuList[selectedItemIndex].Color = Color.White;
                 selectedItemIndex--;
-                if (selectedItemIndex < 0) selectedItemIndex = 0;
+                if (selectedItemIndex < 0) selectedItemIndex = 2;
                 menuList[selectedItemIndex].Color = Color.Red;
             }
            else  if (key == Keyboard.Key.S)
             {
                 menuList[selectedItemIndex].Color = Color.White;
                 selectedItemIndex++;
-                if (selectedItemIndex > 2) selectedItemIndex = 2;
+                if (selectedItemIndex > 2) selectedItemIndex = 0;
                 menuList[selectedItemIndex].Color = Color.Red;
             }
         }
