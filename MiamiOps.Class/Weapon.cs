8e4294551e7fd9051525ba8ebf97b9eb3ab26d47
Alban <dewilde@intechinfo.fr>
2018-04-17 12:21:58 +0200

    the begin of the wepon
    Make the begin of the Weapon constructor and all the weapon in Weapon factory in NotImplementedException().

--- MiamiOps.Class/Weapon.cs
+++ MiamiOps.Class/Weapon.cs
@@ -4,12 +4,69 @@ namespace MiamiOps
 {
     public class Weapon
     {
+        Player _owner;
+        float _attack;
+        float _radius;    // rayon d'action
+        float _range;    // la portée
 
-        public Weapon()
+        public Weapon(Player owner, float attack, float radius, float range)
         {
+            float[] stats = new float[3]{attack, radius, range};
+            foreach (float nb in stats) {if (nb < 0 || nb > 1) {throw new ArgumentException("The parameters can't be lower than 0 or upper than 1.");}}
 
+            this._owner = owner;
+            this._attack = attack;
+            this._radius = radius;
+            this._range = range;
         }
 
+        public void Attack()
+        {
+            throw new NotImplementedException();
+        }
+    }
 
+
+    public class WeaponFactory
+    {
+        public Weapon CreateAssaultRifle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Weapon CreateShotgun()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Weapon CreatePistol()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Weapon CreateBaseballBat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Weapon CreateSoulcalibur()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Weapon CreateChaosBlade()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Weapon CreateGodBlade()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Weapon CreateCompanion()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
