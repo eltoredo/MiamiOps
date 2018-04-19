f67726aca704eec3e76e92c1a858ebfb8141e04f
ElToredo <ascftgbhujj@gmail.com>
2018-04-19 12:10:13 +0200

    Started to implement 2 functions and changed a parameter

--- MiamiOps.Class/Weapon.cs
+++ MiamiOps.Class/Weapon.cs
@@ -9,28 +9,31 @@ namespace MiamiOps
         float _radius;    // rayon d'action
         float _range;    // la portée
         uint _ammo;    // le nombre de fois où tu peux attaquer
+        uint _maxAmmo;
 
-        public Weapon(Player owner, float attack, float radius, float range, uint ammo)
+        public Weapon(Player owner, float attack, float radius, float range, uint _maxAmmo)
         {
             float[] stats = new float[3]{attack, radius, range};
             foreach (float nb in stats) {if (nb < 0 || nb > 1) {throw new ArgumentException("The parameters can't be lower than 0 or upper than 1.");}}
-            if (ammo < 0) {throw new ArgumentException("The ammo can't be lower than zero.");}
+            if (_maxAmmo < 0) {throw new ArgumentException("The max ammo can't be lower than zero.");}
 
             this._owner = owner;
             this._attack = attack;
             this._radius = radius;
             this._range = range;
-            this._ammo = ammo;
+            this._ammo = _maxAmmo;
+            this._maxAmmo = _maxAmmo;
         }
 
         public void Attack()
         {
-            throw new NotImplementedException();
+            //if (_maxAmmo > 0) RemoveFiredBullets();
+            if (_ammo <= 0) Reload();
         }
 
         public void Reload()
         {
-            throw new NotImplementedException();
+            _ammo = _maxAmmo;
         }
     }
 
@@ -39,17 +42,17 @@ namespace MiamiOps
     {
         public Weapon CreateAssaultRifle(Player owner)
         {
-            return new Weapon(owner, 0, 0, 0, 0);
+            return new Weapon(owner, 0, 0, 0, 30);
         }
 
         public Weapon CreateShotgun(Player owner)
         {
-            return new Weapon(owner, 0, 0, 0, 0);
+            return new Weapon(owner, 0, 0, 0, 8);
         }
 
         public Weapon CreatePistol(Player owner)
         {
-            return new Weapon(owner, 0, 0, 0, 0);
+            return new Weapon(owner, 0, 0, 0, 12);
         }
 
         public Weapon CreateBaseballBat(Player owner)
