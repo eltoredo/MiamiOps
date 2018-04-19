42c4bf2b43cacc9edbfbb9662d60bbfbef9745e1
Alban <dewilde@intechinfo.fr>
2018-04-19 09:43:43 +0200

    Create helpers for the player change his weapon

--- MiamiOps.Class/Weapon.cs
+++ MiamiOps.Class/Weapon.cs
@@ -8,19 +8,27 @@ namespace MiamiOps
         float _attack;
         float _radius;    // rayon d'action
         float _range;    // la portée
+        uint _ammo;    // le nombre de fois où tu peux attaquer
 
-        public Weapon(Player owner, float attack, float radius, float range)
+        public Weapon(Player owner, float attack, float radius, float range, uint ammo)
         {
             float[] stats = new float[3]{attack, radius, range};
             foreach (float nb in stats) {if (nb < 0 || nb > 1) {throw new ArgumentException("The parameters can't be lower than 0 or upper than 1.");}}
+            if (ammo < 0) {throw new ArgumentException("The ammo can't be lower than zero.");}
 
             this._owner = owner;
             this._attack = attack;
             this._radius = radius;
             this._range = range;
+            this._ammo = ammo;
         }
 
         public void Attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reload()
         {
             throw new NotImplementedException();
         }
---
+++ MiamiOps.Tests/MiamiOpsWeaponTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+
+
+namespace MiamiOps.Tests
+{
+    [TestFixture]
+    class MiamiOpsWeaponTest
+    {
+        [TestCase("C", 1, "D")]
+        [TestCase("C", -1, "B")]
+        [TestCase("A", -1, "F")]
+        [TestCase("F", 1, "A")]
+        [TestCase("C", -4, "E")]
+        [TestCase("E", 3, "B")]
+        public void Test_the_list_helpers__the_change_element_class_method(string current, int shift, string expected)
+        {
+            List<string> letters = new List<string>{"A", "B", "C", "D", "E", "F"};
+            string letter = letters.OtherElem(current, shift);
+            Assert.That(letter, Is.EqualTo(expected));
+        }
+    }
+}
