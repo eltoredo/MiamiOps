4cbf40d6e4a6d8f4933073037c4b3f28f108a221
ElToredo <ascftgbhujj@gmail.com>
2018-05-23 14:12:25 +0200

    Basic PackageUI class

---
+++ MiamiOps.Ui/PackageUI.cs
@@ -0,0 +1,32 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace MiamiOps
+{
+    public class PackageUI
+    {
+        RoundUI _roundUIContext;
+
+        Texture _packageTexture;
+        Sprite _packageSprite;
+
+        public PackageUI(RoundUI roundUIContext, Texture packageTexture, Vector packagePlace, uint mapWidth, uint mapHeight)
+        {
+            _roundUIContext = roundUIContext;
+
+            _packageTexture = packageTexture;
+            _packageSprite = new Sprite(_packageTexture);
+        }
+
+        public void Draw(RenderWindow window, uint mapWidth, uint mapHeight)
+        {
+            //this._packageSprite.Position = x;
+            _packageSprite.Draw(window, RenderStates.Default);
+        }
+
+        public Vector2f PackagePosition
+        {
+            get { return _packageSprite.Position; }
+        }
+    }
+}
