9d1dc02d575ac362bc046803d18aa93f1a0c7923
ElToredo <ascftgbhujj@gmail.com>
2018-05-25 11:18:22 +0200

    We can change our weapon with the letter P (to check if shift is the right thing to use), the sprite should change too (to check why weapons[0] doesn't work)

--- MiamiOps.Ui/RoundUI.cs
+++ MiamiOps.Ui/RoundUI.cs
@@ -12,6 +12,8 @@ namespace MiamiOps
         Game _gameCtx;
         Map _mapCtx;
         Texture _monsterTexture = new Texture("../../../../Images/monstersprite.png");
+        Texture _weaponTexture;
+        Texture _bulletTexture;
 
         uint _mapWidth;
         uint _mapHeight;
@@ -47,9 +49,17 @@ namespace MiamiOps
 
         public RoundUI(Round roundCtx, Game gameCtx, uint mapWidth, uint mapHeight, Map mapCtx)
         {
-           
-            Texture _closeRangeWeaponTexture = new Texture("../../../../Images/weaponsprite.png");
-            Texture _bulletTexture = new Texture("../../../../Images/fireball.png");
+            // Si c'est l'arme 1 soit le fusil d'assaut
+            //if (_roundCtx.Player.CurrentWeapon == _roundCtx.Player.Weapons[0])
+            //{
+                Texture _weaponTexture = new Texture("../../../../Images/weaponsprite.png");
+                Texture _bulletTexture = new Texture("../../../../Images/fireball.png");
+            //}
+            /*else if (_roundCtx.Player.CurrentWeapon == _roundCtx.Player.Weapons[1])
+            {
+                Texture _weaponTexture = new Texture("../../../../Images/weaponsprite.png");
+                Texture _bulletTexture = new Texture("../../../../Images/fireball.png");
+            }*/
 
             Random _random = new Random();
 
@@ -65,7 +75,7 @@ namespace MiamiOps
                 _enemies[i] = new EnemiesUI(this, _monsterTexture, 4, 54, 48, _roundCtx.Enemies[i].Place, mapWidth, mapHeight, mapCtx);
             }
 
-            _weaponUI = new WeaponUI(this, _closeRangeWeaponTexture, _bulletTexture, _roundCtx.Player.Place, mapWidth, mapHeight);
+            _weaponUI = new WeaponUI(this, _weaponTexture, _bulletTexture, _roundCtx.Player.Place, mapWidth, mapHeight);
 
             _mapWidth = mapWidth;
             _mapHeight = mapHeight;
