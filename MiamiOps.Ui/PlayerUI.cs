585984bd86db98030c54285086675688840563f2
DevilsK <paul.simon7991@gmail.com>
2018-05-31 10:15:05 +0200

    IF WE PRESSES THE BUTTON TU GO UP WE GO UP AND AT THE TOP OF THE MAP THE Y AXE IS POSITIF
    WE DID IT !
    (with Dan, Paul, Alban)

--- MiamiOps.Ui/PlayerUI.cs
+++ MiamiOps.Ui/PlayerUI.cs
@@ -24,7 +24,17 @@ namespace MiamiOps
         FloatRect _hitBoxPlayer;
         Color colorCharacters = new Color(255, 255, 255, 255);
 
-        public PlayerUI(RoundUI roundUIContext, int levelTexture, int nbSprite, int spriteWidth, int spriteHeight, Vector playerPlace, uint mapWidth, uint mapHeight,Map ctxMap)
+        public PlayerUI(
+            RoundUI roundUIContext,
+            int levelTexture,
+            int nbSprite,
+            int spriteWidth,
+            int spriteHeight,
+            Vector playerPlace,
+            uint mapWidth,
+            uint mapHeight,
+            Map ctxMap
+        )
         {
             _roundUIContext = roundUIContext;
             _player = _roundUIContext.RoundContext.Player;
@@ -49,7 +59,7 @@ namespace MiamiOps
         private Vector2f UpdatePlace(uint mapWidth, uint mapHeight)
         {
             _nbDirection = Conversion(_roundUIContext.RoundContext.Player.Direction);
-            Vector2f newPlayerPlace = new Vector2f(((float)_player.Place.X + 1) * (mapWidth / 2), ((float)_player.Place.Y + 1) * (mapHeight / 2));
+            Vector2f newPlayerPlace = new Vector2f(((float)this._player.Place.X + 1) * (mapWidth / 2), (((float)this._player.Place.Y - 1) * (mapHeight / 2)) * (-1));
             
             return newPlayerPlace;
         }
@@ -66,8 +76,9 @@ namespace MiamiOps
             ++_animFrames;
          
             _playerSprite.Draw(window, RenderStates.Default);
-            //Console.WriteLine("x : " + _player.Place.X);
-            //Console.WriteLine("y : " + _player.Place.Y);
+
+            Console.WriteLine("x : " + _player.Place.X);
+            Console.WriteLine("y : " + _player.Place.Y);
         }
 
         private int Conversion(Vector vector)
