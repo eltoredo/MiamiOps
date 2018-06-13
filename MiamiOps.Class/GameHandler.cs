28a3336d362c0667cd7a0bd8322ddc50c238b89c
ElToredo <ascftgbhujj@gmail.com>
2018-06-13 11:45:41 +0200

    Added a new round firstly in GameHandler

--- MiamiOps.Class/GameHandler.cs
+++ MiamiOps.Class/GameHandler.cs
@@ -9,6 +9,8 @@
         {
             _roundObject = roundObject;
             _convert = convert;
+
+            _roundObject = new Round(this, 10, enemieSpawn: new Vector(), enemiesSpeed: 0.0005f, playerSpeed: 0.005f, enemySpawn: _convert.ConvertXMLSpawn(@"..\..\..\..\MiamiOps.Map\Map\tilemap.tmx"), playerHauteur: 0f, playerLargeur: 0f, playerLife: 100);
         }
 
         public void OnLeaving()
